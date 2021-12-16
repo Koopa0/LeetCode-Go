01d68f8ba5c536ca112ecc802d196f9fd89d6b2b
Koopa <koopapapa@gmail.com>
2021-12-16 15:43:17 +0800

    PalindromeNumber

--- PalindromeNumber/main.go
+++ PalindromeNumber/main.go
@@ -1,1 +1,36 @@
-package PalindromeNumber
+package main
+
+import "fmt"
+
+func main() {
+	//驗證
+	if isPalindrome(12221) {
+		fmt.Println("current!")
+	}
+}
+
+func isPalindrome(x int) bool {
+	if x < 0 {
+		return false
+	}
+	if x == 0 {
+		return true
+	}
+	if x%10 == 0 {
+		return false
+	}
+	arr := make([]int, 0, 32)
+	for x > 0 {
+		arr = append(arr, x%10)
+		x = x / 10
+	}
+
+	sz := len(arr)
+
+	for i, j := 0, sz-1; i <= j; i, j = i+1, j-1 {
+		if arr[i] != arr[j] {
+			return false
+		}
+	}
+	return true
+}
